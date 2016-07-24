4c4f7707c01973b87c5cf7b62064b320cc6b6e4d
kennelbound <sam@kennelbound.com>
2016-07-24 02:21:47 -0700

    Deployment fixed.  Making it easier to launch Dynamo without thinking about it.

--- dynamodb.local.js
+++ dynamodb.local.js
@@ -5,7 +5,11 @@ var exec = require('child_process').exec;
 var port = env.DB_PORT || 8001;
 
 // Execute some command with process.env and my custom variables
-var command = 'java -Djava.library.path=DynamoDBLocal_lib -jar DynamoDBLocal/DynamoDBLocal.jar --port ' + port;
+var command = 'java -Djava.library.path=DynamoDBLocal_lib -jar DynamoDBLocal/DynamoDBLocal.jar" +' +
+    ' -port ' + port +
+    ' -optimizeDbBeforeStartup' +
+    ' -dbPath DynamoDBLocal';
+console.log("Executing " + command);
 var child = exec(command, {env: env}, function (err, stdout, stderr) {
     if (err) {
         throw err;
@@ -20,7 +24,8 @@ child.stderr.pipe(process.stderr);
 module.exports = function (keep_process_alive) {
     // Keep the thread alive forever
     if (keep_process_alive) {
-        setInterval(function () { }, Math.POSITIVE_INFINITY);
+        setInterval(function () {
+        }, Math.POSITIVE_INFINITY);
     }
     return child;
 };
