263c2266b20622c870f20a14bdf0a47b03062268
kennelbound <sam@kennelbound.com>
2016-07-24 01:29:50 -0700

    Adding a script that will launch the dynamodblocal instance for you iin a child process.

---
+++ dynamodb.local.js
@@ -0,0 +1,28 @@
+var env = process.env;
+
+var exec = require('child_process').exec;
+
+var port = env.DB_PORT || 8001;
+
+// Execute some command with process.env and my custom variables
+var command = 'java -Djava.library.path=DynamoDBLocal_lib -jar DynamoDBLocal/DynamoDBLocal.jar --port ' + port;
+var child = exec(command, {env: env}, function (err, stdout, stderr) {
+    if (err) {
+        throw err;
+    }
+    // console.log(stdout);
+    // console.error(stderr);
+});
+
+child.stdout.pipe(process.stdout);
+child.stderr.pipe(process.stderr);
+
+module.exports = function (keep_process_alive) {
+    // Keep the thread alive forever
+    if (keep_process_alive) {
+        setInterval(function () { }, Math.POSITIVE_INFINITY);
+    }
+    return child;
+};
+
+
